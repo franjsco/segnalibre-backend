e86c62e5bb2847750e58b516fcb15dde12702d6f
Francesco Esposito <33671357+frsposito@users.noreply.github.com>
2019-11-23 14:06:08 +0100

    edit admin

--- segnalibre_app/admin.py
+++ segnalibre_app/admin.py
@@ -2,4 +2,8 @@ from django.contrib import admin
 from .models import Book
 
 # Register your models here.
-admin.site.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'author', 'owner')
+
+
+admin.site.register(Book, BookAdmin)
