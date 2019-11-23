5e7f9c94ceb2fb0d9db8a40eca3f851974214305
Francesco Esposito <33671357+frsposito@users.noreply.github.com>
2019-11-23 12:25:13 +0100

    remove django filters (module)

--- segnalibre/settings.py
+++ segnalibre/settings.py
@@ -39,7 +39,6 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'corsheaders',
-    'django_filters',
     'segnalibre_app'
 ]
 
@@ -130,6 +129,5 @@ CORS_ORIGIN_WHITELIST = [
 
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 10,
-    'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend']
+    'PAGE_SIZE': 10
 }
