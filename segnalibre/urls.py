903e806761adf15f90959110b0bff8e6d9feec72
Francesco Esposito <33671357+frsposito@users.noreply.github.com>
2019-11-23 12:24:50 +0100

    edit baseurl

--- segnalibre/urls.py
+++ segnalibre/urls.py
@@ -20,5 +20,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('segnalibre_app.urls'))
+    path('api/', include('segnalibre_app.urls'))
 ]
