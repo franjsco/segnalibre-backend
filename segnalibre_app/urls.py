f8ed0630f21c23a4ed823c94b038be4f4bb3f4fc
Francesco Esposito <33671357+frsposito@users.noreply.github.com>
2019-11-23 12:22:24 +0100

    edit routes

--- segnalibre_app/urls.py
+++ segnalibre_app/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path, include
 from . import views
 
+prefix = 'v1/'
+
 urlpatterns = [
-    path('users/', views.UserList.as_view()),
-    path('users/<int:pk>/', views.UserDetail.as_view()),
-    path('books/', views.BookList.as_view()),
-    path('books/<int:pk>/', views.BookDetail.as_view()),
+    path(prefix + 'users/', views.UserList.as_view()),
+    path(prefix + 'books/', views.BookList.as_view()),
+    path(prefix +'books/<int:pk>/', views.BookDetail.as_view()),
     path('api-auth/', include('rest_framework.urls'))
 ]
