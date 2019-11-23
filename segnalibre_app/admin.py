6b09482873122badedbf558a08df88ba0b2add57
Francesco Esposito <33671357+frsposito@users.noreply.github.com>
2019-11-23 12:24:37 +0100

    edit admin

--- segnalibre_app/admin.py
+++ segnalibre_app/admin.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
-from .models import Book, Bookmark
+from .models import Book
 
 # Register your models here.
 admin.site.register(Book)
-admin.site.register(Bookmark)
