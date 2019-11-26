a181de664fd13755fabab7d738c25bf3dc3648d6
Francesco Esposito <33671357+frsposito@users.noreply.github.com>
2019-11-26 10:04:18 +0100

    Implement JWT Authentication

--- segnalibre/settings.py
+++ segnalibre/settings.py
@@ -128,6 +128,12 @@ CORS_ORIGIN_WHITELIST = [
 ]
 
 REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework_simplejwt.authentication.JWTAuthentication'
+    ],
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated'
+    ),
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10
 }
--- segnalibre_app/urls.py
+++ segnalibre_app/urls.py
@@ -1,5 +1,9 @@
 from django.urls import path, include
 from . import views
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+    TokenVerifyView)
 
 prefix = 'v1/'
 
@@ -7,5 +11,8 @@ urlpatterns = [
     path(prefix + 'users/', views.UserList.as_view()),
     path(prefix + 'books/', views.BookList.as_view()),
     path(prefix +'books/<int:pk>/', views.BookDetail.as_view()),
-    path('api-auth/', include('rest_framework.urls'))
+    path('api-auth/', include('rest_framework.urls')),
+    path(prefix + 'auth/token', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path(prefix + 'auth/refresh', TokenRefreshView.as_view(), name='token_refresh'),
+    path(prefix + 'auth/verify', TokenVerifyView.as_view(), name='token_verify')
 ]
