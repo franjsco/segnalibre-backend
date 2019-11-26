6c27cbe7c1197034d95e335e3e25d166e8e21350
Francesco Esposito <33671357+frsposito@users.noreply.github.com>
2019-11-26 13:07:50 +0100

    add python-dotenv

--- segnalibre/settings.py
+++ segnalibre/settings.py
@@ -11,16 +11,17 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+from dotenv import load_dotenv
 
+load_dotenv()
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'secret' # os environment
+SECRET_KEY = os.getenv('SEGNALIBRE_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
